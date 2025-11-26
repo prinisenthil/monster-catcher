b82d21c2ae32a33c20c6ecd0c1d4dcf7924f46f2
prinisenthil <prini.senthil101@gmail.com>
2025-11-25 21:06:46 -0500

    fixing nurse textbox

--- scripts/nurse.gd
+++ scripts/nurse.gd
@@ -49,13 +49,11 @@ func _input(event):
 		emit_signal("textbox_closed")
 
 # Displays text in the dialogue box
-func display_texting(text):
-	$UI/Textbox.show()
-	await $UI/Textbox/Dialogue.type_text(text)
-
 func display_text(text):
 	$UI/Textbox.show()
-	type_text(text)
+	await $UI/Textbox/Dialogue.type_text(text)
+	await get_tree().create_timer(1).timeout
+	await self.textbox_closed
 
 # Called when the player enters the interaction zone around the nurse
 func _on_interaction_zone_body_entered(body: Node2D) -> void:
@@ -67,15 +65,3 @@ func _on_interaction_zone_body_exited(body: Node2D) -> void:
 	if body.name == "player":
 		can_interact = false # Disable interaction
 		dialogue_label.visible = false  # Hide any open dialogue
-
-func type_text(text):
-	var label = $UI/Textbox/Dialogue
-	label.text = ""
-	for i in range(text.length()):
-		label.text += text[i]
-		await get_tree().create_timer(0.03).timeout
-		if Input.is_action_just_pressed("ui_accept"):
-			label.text = text
-			await get_tree().create_timer(1).timeout
-			break
-	await get_tree().create_timer(0.5).timeout
